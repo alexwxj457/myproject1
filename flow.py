2cb3e5ca8780d21e6fbe52e859be634362b934a3
王兴结 <2112204405@mail2.gdut.edu.cn>
2023-07-03 16:06:03 +0800

    111

--- flow.py
+++ flow.py
@@ -3,7 +3,7 @@ import traci
 # 生成车辆或车流
 def  flow(num,car):#生成的车辆数和车辆类型
 # 生成包含90个1和10个0的列表
-    my_list = [1] * num + [0] * (60-num)#每分钟生成的车辆数，1为生成，0不生成
+    my_list = [1] * num + [0] * (600-num)#每分钟生成的车辆数，1为生成，0不生成
     n=0
     time=0#车辆生成时间
 # 随机打乱列表中元素的顺序
@@ -12,7 +12,7 @@ def  flow(num,car):#生成的车辆数和车辆类型
         if i==1:
            vehicle_id = f"{car}{n}"#车辆名称
            n+=1
-           traci.vehicle.add(vehicle_id, "r0", typeID=car,depart=time, departLane="best",departSpeed=10)#生成车辆
+           traci.vehicle.add(vehicle_id, "r0", typeID=car,depart=time, departLane="best",departSpeed=20)#生成车辆
         time+=1
 
 
--- sumo.py
+++ sumo.py
@@ -11,10 +11,13 @@ traci.start(sumoCmd)
 #交叉口名称
 junction_id = "J2"
 #设置车流，10为数量
-flow(10,car="car")
+flow(100,car="car")
 # 设置公交车
-flow(2,car="bus")
-    # depart_time += 1  # 调整出发时间
+# flow(5,car="bus")
+traci.vehicle.add("bus0", "r0", typeID="bus",depart=20, departLane="best",departSpeed=20)#生成车辆
+traci.vehicle.add("bus1", "r0", typeID="bus",depart=60, departLane="best",departSpeed=20)#生成车辆
+traci.vehicle.add("bus32", "r0", typeID="bus",depart=160, departLane="best",departSpeed=20)#生成车辆
+    # depart_time +=b 1  # 调整出发时间
 # 添加车辆
 simulation_steps = 88888  # 总共运行的仿真步数
 time=0
@@ -26,45 +29,58 @@ phase_list = ["rrrrrGGGrrrrrrrGGGrr", "rrrrrrrrGGrrrrrrrrGG", "GGGrrrrrrrGGGrrrr
 phase_time_list = [33, 15, 33, 15]
 light_set, time_set = light(phase_list, phase_time_list)
 print(time_set)
-in_put=int(input("优先时间"))
+# in_put=int(input("优先时间"))
+ti=0
+mmm=0
+lm_t=0
 for step in range(simulation_steps):
     traci.simulationStep()
     queue_length = traci.lanearea.getLastStepHaltingNumber("e2det_-E7_2")#道路检测器
-    # print("Queue Length:", queue_length)
     time0 = traci.simulation.getCurrentTime()#获取仿真时间，ms
     time1=time0/1000#转化为s
     time = time1%time_set[-1]#确定周期数
-    # if t==0:
     try:
-        vehicle_position = traci.vehicle.getPosition("car0")#获取车辆位置信息
-        # print(vehicle_position[0])
+        if mmm==0:
+            vehicle_position0 = traci.vehicle.getPosition("bus0")#获取车辆位置信息
+            v0=vehicle_position0[0]
+        if mmm<=1:
+            vehicle_position1=traci.vehicle.getPosition("bus1")
+            v1=vehicle_position1[0]
+            vehicle_position=min([x for x in [-100-v0,-100-v1] if x>-100])
     except:
-        vehicle_position=1000000000#检测不到车辆时，距离为无穷大
+        if mmm==0:
+            v0=1000000
+        if mmm==1:
+            vehicle_position=10000
+        mmm+=1
+        pass
     # tls_state = traci.trafficlight.getRedYellowGreenState(junction_id)#获取信号灯
     # print(vehicle_position[0])
-    if time==in_put:
-        sg=1
+    tls_state = traci.trafficlight.getRedYellowGreenState(junction_id)
+    print(vehicle_position, tls_state[6], ti)
+    if -20<vehicle_position<10 and tls_state[6] =="r" and ti==0:
+        sg=1#控制优先信号打开
+        ti=1#保证一个周期内只进行一次优化
+    if ti==1:
+        print(lm_t,ti)
+        lm_t+=1
+        if lm_t==time_set[-1]:
+            ti=0
+            lm_t=0
     if sg==0:
         closest_number = max([x for x in time_set if x <= time])#获取时间列表内当前时间（time）左侧的第一个时间
         tls_state=light_set[time_set.index(closest_number)]#找到对应位置的相位
+
     if sg==1:
-        y+=1#用于获取优化时刻的时间
-        if y==1:
+        if y==0:
             a=time
-            new_light,new_time=youxian(a,6,youxian_light='rrrrrrrrGGrrrrrrrrGG',light_list=light_set,time_list=time_set)
-        # t=time_set[light_set.index('rrrrrrrrGGrrrrrrrrGG')+int(len(light_set)/2)]
+            y+=1
+            new_light,new_time=youxian(a,8,youxian_light='rrrrrGGGrrrrrrrGGGrr',light_list=light_set,time_list=time_set)
         closest_number = max([x for x in new_time if x <= time])
         tls_state = new_light[new_time.index(closest_number)]#同上
-        if time==new_time[-1]-1:#优化结束，复位
-            y=0
+        if time==new_time[-1]-1 :#优化结束，复位
             sg=0
-
-    # speed = traci.vehicle.getSpeed("vehicle1")
-    # if speed<0:
-    #     print("Vehicle speed:", 0)
-    # else:
-    #     print("Vehicle speed:", speed)
-        # 改变信号灯状态
+            y=0
     traci.trafficlight.setRedYellowGreenState(junction_id, tls_state)#获取信号灯状态
 
 traci.close()
